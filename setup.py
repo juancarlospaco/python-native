cea47ba833ee2a6a46b4fbe468bbff33acc54ffc
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-23 22:04:31 -0300

    init

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ git_repo = "https://github.com/juancarlospaco/python-native.git"
 
 assert platform.architecture()[0] == "64bit", "ERROR: Python must be 64 Bit!. OS must be 64 Bit!."
 assert sys.version_info > (3, 5, 0), "ERROR: Python version must be > 3.5!."
-class CustomInstall(install):
+class X(install):
   def run(self):
     install.run(self)
     subprocess.call("nimble --accept --noSSLCheck install " + git_repo)
 
-setup(cmdclass={"install": CustomInstall})
+setuptools.setup(cmdclass = {"install": X})
